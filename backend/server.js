2c2023be77075cfaa300de9efc306a5a3d81d6c6
animenzo <animenzo941@gmail.com>
2025-03-09 16:21:29 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,14 @@ const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const cors = require("cors");
 const router = express.Router();
+const path = require("path");
 
 dotenv.config();
 connectDB();
 
 const app = express();
+
+
 app.use(express.json());
 app.use(
 	cors({
@@ -22,4 +25,14 @@ app.use("/api/auctions", require("./routes/auctionRoutes"));
 app.use("/api/bids", require("./routes/bidRoutes"));
 
 const PORT = process.env.PORT || 5000;
+const _dirname = path.resolve();
+app.use(express.static(path.join(_dirname, "/frontend/dist")));
+app.get("*", (req,res)=>{
+	res.sendFile(path.resolve(_dirname, "frontend", "dist", "index.html"))
+})
+
+
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
